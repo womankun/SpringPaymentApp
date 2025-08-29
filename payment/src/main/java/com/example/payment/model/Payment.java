ac6adc5fe658d0f2373c0b84f0c0364380799427
womankun <renta.lsb.soccer@gmail.com>
2025-08-29 10:13:17 +0900

    Lombokの記述を簡潔に

--- payment/src/main/java/com/example/payment/model/Payment.java
+++ payment/src/main/java/com/example/payment/model/Payment.java
@@ -4,19 +4,14 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
+@Data
 public class Payment {
     private final String paymentId;
-    @Setter
     private BigDecimal amount;
-    @Setter
     private String status;
-    @Setter
     private String cardNumber;
-    @Setter
     private String cardExpiry;
     private Timestamp createdAt;
     

838972cc08c92ecc832f64f3d04a2f91b8ad8dae
womankun <renta.lsb.soccer@gmail.com>
2025-09-04 18:20:09 +0900

    payment-recordAPIにおいてクロスオリジンの許可

--- payment/src/main/java/com/example/payment/controller/PaymentRecordController.java
+++ payment/src/main/java/com/example/payment/controller/PaymentRecordController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payment-records")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class PaymentRecordController {
 
     private final PaymentRecordService paymentRecordService;
